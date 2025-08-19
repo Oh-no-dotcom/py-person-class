c1d43634ffeddcd53242bb1e52f36e36b334578e
Dmytro Shulha <dimonchik0218@gmail.com>
2025-08-19 21:10:38 +0300

    solved Class Person task

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for dict_num in people:
+        Person(dict_num["name"], dict_num["age"])
+    for dict_num in people:
+        if dict_num.get("wife") is not None:
+            Person.people[dict_num["name"]].wife \
+                = Person.people[dict_num["wife"]]
+        if dict_num.get("husband") is not None:
+            Person.people[dict_num["name"]].husband \
+                = Person.people[dict_num["husband"]]
+    return [Person.people[dict_num["name"]] for dict_num in people]
